dd6300130625c19485f87e9aa55f030969519920
Mehmet ALTAN <77855984+mehmetaltann@users.noreply.github.com>
2023-08-10 16:43:23 +0300

    Add files via upload

---
+++ lesson1/client/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import "./styles/styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
